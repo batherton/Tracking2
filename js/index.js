480dda6dfdb5923713027c341d0462f58658924c
batherton <brian@asimail.com>
2015-08-12 12:03:00 -0400

    8

--- js/index.js
+++ js/index.js
@@ -106,19 +106,13 @@ var app = {
     			// If you wish to turn OFF background-tracking, call the #stop method.
     			// bgGeo.stop()
 
-
-
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
-
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
     }
 };
